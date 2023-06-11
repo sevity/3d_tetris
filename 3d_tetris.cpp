9fcf42e4295e9e1a10296db255c2496090eab2d5
Wonil Kim <sevity@ymail.com>
2023-06-11 20:39:27 +0900

    outline done

--- 3d_tetris.cpp
+++ 3d_tetris.cpp
@@ -5,9 +5,69 @@
 #include <sstream>
 #include <vector>
 #include <numeric>
+#include <map>
+#include <algorithm>
+#include <set>
+using namespace std;
 
 typedef sf::Vector3f point_3d;
 typedef sf::Vector2f point_2d;
+
+int blocks[7][3][3][3] = {
+    0,0,0, // 천장층
+    1,1,0,
+    1,1,0,
+
+    0,0,0, // 중간층
+    0,0,0,
+    0,0,0,
+
+    0,0,0, // 바닥층
+    0,0,0,
+    0,0,0,
+};
+int world[10][3][3] = {
+    0,0,0,
+    0,0,0,
+    0,0,0,
+
+    0,0,0,
+    0,0,0,
+    0,0,0,
+
+    0,0,0,
+    0,0,0,
+    0,0,0,
+
+    0,0,0,
+    0,0,0,
+    0,0,0,
+
+    0,0,0,
+    0,0,0,
+    0,0,0,
+
+    0,0,0,
+    0,0,0,
+    0,0,0,
+
+    0,0,0,
+    0,0,0,
+    0,0,0,
+
+    0,1,0,
+    0,0,0,
+    0,0,1,
+
+    0,1,1,
+    0,0,0,
+    0,0,1,
+
+    1,1,1,
+    0,1,1,
+    1,0,1,
+};
+
 
 sf::Color lv_color[] = {sf::Color::Red, sf::Color::Cyan, sf::Color::Green, sf::Color::Blue, sf::Color(128, 128, 128), sf::Color::Yellow, sf::Color::Magenta, sf::Color::Red,
                      sf::Color::Cyan, sf::Color::Green, sf::Color::Blue};
@@ -78,6 +138,7 @@ bool loadObjFile(const std::string& filename, std::vector<sf::Vector3f>& vertice
     file.close();
     return true;
 }
+int g_cnt = 0;
 void processKeyInput(sf::Event& event, float& angleX, float& angleY, float& angleZ) {
     if (event.type == sf::Event::KeyPressed) {
         switch (event.key.code) {
@@ -87,6 +148,7 @@ void processKeyInput(sf::Event& event, float& angleX, float& angleY, float& angl
             case sf::Keyboard::A: angleX = -90.0f; break;
             case sf::Keyboard::S: angleY = -90.0f; break;
             case sf::Keyboard::D: angleZ = -90.0f; break;
+            case sf::Keyboard::Z: ++g_cnt; printf("g_cnt:%d\n", g_cnt);break;
             default: break;
         }
     }
@@ -174,7 +236,7 @@ void write_cube(std::vector<sf::Vector3f>& vertices, float x, float y, float z) 
     }
 }
 
-void generate_world_blocks(std::vector<sf::Vector3f>& vertices, std::vector<std::vector<int>>& faces, int world[10][3][3]) {
+void generate_world_blocks(int world[10][3][3], std::vector<sf::Vector3f>& vertices, std::vector<std::vector<int>>& faces) {
     int base_idx = 0;
 
     //for (int z = 0; z < 5; ++z) {
@@ -183,23 +245,148 @@ void generate_world_blocks(std::vector<sf::Vector3f>& vertices, std::vector<std:
             for (int x = 0; x < 3; ++x) {
                 if (world[z][y][x] == 1) {
                     write_cube(vertices, x, y, z);
-                    // 바닥 back face
-                    //faces.push_back({base_idx+2, base_idx+3, base_idx+7, base_idx+6});
-                    // E right face
-                    faces.push_back({base_idx+1, base_idx+2, base_idx+6, base_idx+5});
-                    // W left face
-                    faces.push_back({base_idx, base_idx+3, base_idx+7, base_idx+4});
-                    // S top face
-                    faces.push_back({base_idx+4, base_idx+5, base_idx+6, base_idx+7});
-                    // N bottom face
-                    faces.push_back({base_idx, base_idx+1, base_idx+2, base_idx+3});
-                    // 뚜껑 front face
-                    faces.push_back({base_idx, base_idx+1, base_idx+5, base_idx+4});
+                    //faces.push_back({base_idx+2, base_idx+3, base_idx+7, base_idx+6}); // 바닥 back face
+                    faces.push_back({base_idx+1, base_idx+2, base_idx+6, base_idx+5});   // E right face
+                    faces.push_back({base_idx, base_idx+3, base_idx+7, base_idx+4});     // W left face
+                    faces.push_back({base_idx+4, base_idx+5, base_idx+6, base_idx+7});   // S top face
+                    faces.push_back({base_idx, base_idx+1, base_idx+2, base_idx+3});     // N bottom face
+                    faces.push_back({base_idx, base_idx+1, base_idx+5, base_idx+4});     // 뚜껑 front face
                     base_idx += 8;
                 }
             }
         }
     }
+}
+/*
+void generate_blocks(int blocks[7][3][3][3], std::vector<sf::Vector3f>& vertices, std::vector<std::vector<int>>& faces) {
+    int base_idx = 0;
+
+    for (int z = 2; z >= 0; --z) {
+        for (int y = 0; y < 3; ++y) {
+            for (int x = 0; x < 3; ++x) {
+                if (blocks[0][z][y][x] == 1) {
+                    write_cube(vertices, x, y, z);
+                    //faces.push_back({base_idx+2, base_idx+3, base_idx+7, base_idx+6}); // 바닥 back face
+                    faces.push_back({base_idx+1, base_idx+2, base_idx+6, base_idx+5});   // E right face
+                    faces.push_back({base_idx, base_idx+3, base_idx+7, base_idx+4});     // W left face
+                    faces.push_back({base_idx+4, base_idx+5, base_idx+6, base_idx+7});   // S top face
+                    faces.push_back({base_idx, base_idx+1, base_idx+2, base_idx+3});     // N bottom face
+                    faces.push_back({base_idx, base_idx+1, base_idx+5, base_idx+4});     // 뚜껑 front face
+                    base_idx += 8;
+                }
+            }
+        }
+    }
+}
+*/
+struct Vector3fCompare {
+    bool operator() (const sf::Vector3f& lhs, const sf::Vector3f& rhs) const {
+        return std::tie(lhs.x, lhs.y, lhs.z) < std::tie(rhs.x, rhs.y, rhs.z);
+    }
+};
+
+struct Line {
+    sf::Vector3f v1;
+    sf::Vector3f v2;
+    Line(sf::Vector3f v1, sf::Vector3f v2) : v1(v1), v2(v2) {
+        if (Vector3fCompare()(v2, v1)) std::swap(this->v1, this->v2);
+    }
+};
+
+struct LineCompare {
+    bool operator() (const Line& lhs, const Line& rhs) const {
+        Vector3fCompare comp;
+        return comp(lhs.v1, rhs.v1) ? true : (!comp(rhs.v1, lhs.v1) && comp(lhs.v2, rhs.v2));
+    }
+};
+
+const std::vector<std::vector<int>> cube_lines = {
+    {0, 1}, {1, 2}, {2, 3}, {3, 0},
+    {4, 5}, {5, 6}, {6, 7}, {7, 4},
+    {0, 4}, {1, 5}, {2, 6}, {3, 7}
+};
+
+void generate_blocks(int blocks[7][3][3][3], std::vector<sf::Vector3f>& vertices, std::vector<std::vector<int>>& lines) {
+    int base_idx = 0;
+
+    for (int z = 2; z >= 0; --z) {
+        for (int y = 0; y < 3; ++y) {
+            for (int x = 0; x < 3; ++x) {
+                if (blocks[0][z][y][x] == 1) {
+                    write_cube(vertices, x, y, z);
+                    int cnt = 0;
+                     for (const auto& line : cube_lines) {
+                        lines.push_back({base_idx + line[0], base_idx + line[1]});
+                        //if(++cnt>g_cnt) return;
+                    }
+                    base_idx += 8;
+                }
+            }
+        }
+    }
+
+    vector<vector<point_3d>> lines2;
+    for (const auto& line : lines) {
+        vector<point_3d> vp;
+        vp.push_back(vertices[line[0]]);
+        vp.push_back(vertices[line[1]]);
+        lines2.push_back(vp);
+    }
+
+    std::vector<std::pair<sf::Vector3f, sf::Vector3f>> overlapping_edges;
+    map<int, int> overlap_idx;
+
+    for (size_t i = 0; i < lines2.size(); ++i) {
+        const auto& edge1 = lines2[i];
+        
+        for (size_t j = i + 1; j < lines2.size(); ++j) {
+            const auto& edge2 = lines2[j];
+            
+            if ((edge1[0] == edge2[0] && edge1[1] == edge2[1]) ||
+                (edge1[0] == edge2[1] && edge1[1] == edge2[0])) {
+                overlapping_edges.push_back(std::make_pair(edge1[0], edge1[1]));
+                overlap_idx[i] ++;
+                overlap_idx[j] ++;
+            }
+        }
+    }
+    vector<vector<int>> lines3;
+    for (size_t i = 0; i < lines.size(); ++i) {
+        if(overlap_idx[i]==2 || overlap_idx[i]==0)
+            lines3.push_back(lines[i]);
+    }
+    lines = lines3;
+
+
+    return;
+
+
+
+
+    std::map<Line, int, LineCompare> line_map;
+    for (const auto& line : lines) {
+        Line current_line(vertices[line[0]], vertices[line[1]]);
+        if (line_map.find(current_line) != line_map.end()) {
+            line_map.erase(current_line);
+        } else {
+            line_map[current_line] = 1;
+        }
+    }
+
+    lines.clear();
+    for (const auto& line_map_entry : line_map) {
+        std::vector<int> line_indices(2);
+        for (int i = 0; i < vertices.size(); ++i) {
+            if (vertices[i] == line_map_entry.first.v1) {
+                line_indices[0] = i;
+            }
+            if (vertices[i] == line_map_entry.first.v2) {
+                line_indices[1] = i;
+            }
+        }
+        lines.push_back(line_indices);
+    }
+
 }
 
 int main() {
@@ -215,70 +402,25 @@ int main() {
         vertex.z += 1.5;
     }
 
-    std::vector<sf::Vector3f> v2;
-    std::vector<std::vector<int>> f2;
-    std::vector<std::vector<int>> l2;
-    loadObjFile("obj/t_block_0.obj", v2, f2, l2);
-
-    for (auto& vertex : v2) {
-        vertex.x -= 1.5;
-        vertex.y -= 1.5;
-        vertex.z -= 0.5;
-    }
+    std::vector<sf::Vector3f> vb;
+    std::vector<std::vector<int>> fb;
+    std::vector<std::vector<int>> lb;
+    //loadObjFile("obj/t_block_0.obj", v2, f2, l2);
+    generate_blocks(blocks, vb, lb);
 
     sf::Vector3f center(0, 0, 0);  // 객체의 중심점 정의
+    /*
     center = std::accumulate(v2.begin(), v2.end(), center);
     center /= (float)v2.size();
     center.y-=0.7;
     printf("center: %f, %f, %f\n", center.x, center.y, center.z);
+    */
 
-
-    int world[10][3][3] = {
-        0,0,0,
-        0,0,0,
-        0,0,1,
-
-        0,0,0,
-        0,0,0,
-        0,0,1,
-
-        0,0,0,
-        0,0,0,
-        0,0,1,
-
-        0,0,0,
-        0,0,0,
-        0,0,1,
-
-        0,0,0,
-        0,0,0,
-        0,0,1,
-
-        0,0,0,
-        0,0,0,
-        0,0,1,
-
-        0,0,0,
-        0,0,0,
-        0,0,1,
-
-        0,1,0,
-        0,0,0,
-        0,0,1,
-
-        0,1,1,
-        0,0,0,
-        0,0,1,
-
-        1,1,1,
-        0,1,1,
-        1,0,1,
-    };
 
     std::vector<sf::Vector3f> vertices;
     std::vector<std::vector<int>> faces;
     std::vector<std::vector<int>> lines;
-    generate_world_blocks(vertices, faces, world);
+    generate_world_blocks(world, vertices, faces);
     std::sort(faces.begin(), faces.end(), [&](const std::vector<int>& face1, const std::vector<int>& face2) {
         return compareFaces(face1, face2, vertices);
     });
@@ -290,12 +432,12 @@ int main() {
                 window.close();
             processKeyInput(event, angleX, angleY, angleZ);
         }
-        for (auto& vertex : v2) vertex = rotate(vertex - center, angleX, angleY, angleZ) + center;
+        for (auto& vertex : vb) vertex = rotate(vertex - center, angleX, angleY, angleZ) + center;
 
         window.clear();
-        draw_stage(window, vs, fs);
-        draw_world(window, vertices, faces);
-        draw_block(window, v2, l2);
+        //draw_stage(window, vs, fs);
+        //draw_world(window, vertices, faces);
+        draw_block(window, vb, lb);
         window.display();
     }
 
